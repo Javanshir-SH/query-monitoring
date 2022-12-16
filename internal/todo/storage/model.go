fb97bdd98123da0f144234669d9baa5731629ff5
Javanshir Shabiyev <cavanshir.shabiyev@gmail.com>
2022-12-16 15:54:21 +0400

    :octocat:

--- internal/todo/storage/model.go
+++ internal/todo/storage/model.go
@@ -1,6 +1,14 @@
 package storage
 
+const (
+	TaskTableName = "todos"
+)
+
 type Task struct {
 	ID    int    `gorm:"column:id;primary_key"`
 	Title string `gorm:"column:title"`
+}
+
+func (t Task) TableName() string {
+	return TaskTableName
 }
